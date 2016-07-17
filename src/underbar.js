f80837944ff81ba5329cd60b60b4b362ace7686b
Patrick Hartley <patrickliamhartley@gmail.com>
2016-07-17 03:26:14 -0400

    "Fixed array half of _.each function"

--- src/underbar.js
+++ src/underbar.js
@@ -51,6 +51,20 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+
+      for (var i=0;i<collection.length;i++){
+        var value=collection[i];
+        var key=i;
+        var source = collection;
+        iterator(value, key, source);
+      }
+
+
+
+    }
+
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
