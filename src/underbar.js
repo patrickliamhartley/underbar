ea724fd6cb15b5df9706a5b44bb22396ab25e673
Patrick Hartley <patrickliamhartley@gmail.com>
2016-07-18 04:07:24 -0400

    "Fixed extends and defaults"

--- src/underbar.js
+++ src/underbar.js
@@ -275,15 +275,15 @@
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
       var args= arguments;
-      var newObj={};
+
 
       for( var i=0;i<args.length;i++){
          for (var key in args[i]){
-          newObj[key]=args[i][key];
+          obj[key]=args[i][key];
 
          }
       }
-      return newObj;
+      return obj;
 
 
 
@@ -292,7 +292,24 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+          var args= arguments;
+
+
+      for( var i=0;i<args.length;i++){
+         for (var key in args[i]){
+          if( obj[key]=== undefined){
+
+          obj[key]=args[i][key];
+          }
+
+        }
+      }
+      return obj;
+
+
+
   };
+
 
 
   /**
